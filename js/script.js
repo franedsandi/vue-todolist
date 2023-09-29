b19d38022cd26fff73a70fbe5862f20a682efc47
franedsandi <franedsandi@gmail.com>
2023-09-29 19:30:30 +0200

    [feat] task done line

--- js/script.js
+++ js/script.js
@@ -1,31 +1,44 @@
-const {createApp}= Vue ;
+const { createApp } = Vue;
 
 createApp({
-  data(){
-    return{
-      tasks:[
-        "fare la spesa",
-        "studiare"
-      ],
-      newTask:"",
-      isError:false
-    }
+  data() {
+    return {
+      tasks: [],
+      newTask: "",
+      isError: false,
+      completedTasks: [] // Nueva propiedad para el seguimiento de tareas completadas
+    };
   },
-  methods:{
-    addNewTask(){
-      if(this.newTask.length < 5){
-        this.isError=true;
+  methods: {
+    addNewTask() {
+      if (this.newTask.length < 5) {
+        this.isError = true;
         setTimeout(() => {
           this.isError = false;
         }, 1500);
-      }else{
-      this.tasks.unshift(this.newTask)
-      this.newTask="";
-      this.isError=false
+      } else {
+        this.tasks.unshift(this.newTask);
+        this.newTask = "";
+        this.isError = false;
       }
     },
-    removeTask(index){
-      this.tasks.splice(index,1)
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+      // Si la tarea se elimina, también la eliminamos de la lista de completadas
+      const taskIndex = this.completedTasks.indexOf(index);
+      if (taskIndex !== -1) {
+        this.completedTasks.splice(taskIndex, 1);
+      }
+    },
+    toggleTask(index) {
+      if (this.completedTasks.includes(index)) {
+        // Si la tarea ya está marcada como completada, la eliminamos de la lista de completadas
+        const taskIndex = this.completedTasks.indexOf(index);
+        this.completedTasks.splice(taskIndex, 1);
+      } else {
+        // Si la tarea no está marcada como completada, la agregamos a la lista de completadas
+        this.completedTasks.push(index);
+      }
     }
   }
-}).mount ('#app')
+}).mount("#app");
