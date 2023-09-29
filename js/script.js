d5816acc210adc2b13d7654c2e9e304ae50f1477
franedsandi <franedsandi@gmail.com>
2023-09-29 19:02:42 +0200

    [feat] error message

--- js/script.js
+++ js/script.js
@@ -7,13 +7,22 @@ createApp({
         "fare la spesa",
         "studiare"
       ],
-      newTask:""
+      newTask:"",
+      isError:false
     }
   },
   methods:{
     addNewTask(){
+      if(this.newTask.length < 5){
+        this.isError=true
+      }else{
       this.tasks.unshift(this.newTask)
-      this.newTask=""
+      this.newTask="";
+      this.isError=false
+      }
+    }
+    removeTask(){
+      
     }
   }
 }).mount ('#app')
